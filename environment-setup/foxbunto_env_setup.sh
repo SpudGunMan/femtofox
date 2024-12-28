fc84737450105d172801d654b7d791b493287437
SpudGunMan <kellykeeton@yahoo.com>
2024-12-28 15:28:03 -0800

    Update foxbunto_env_setup.sh

--- environment-setup/foxbunto_env_setup.sh
+++ environment-setup/foxbunto_env_setup.sh
@@ -211,7 +211,6 @@ echo "Installing packages..."
 # link gcc library
 ln -sf /lib/arm-linux-gnueabihf/libgcc_s.so.1 /usr/lib/libgcc.so
 DEBIAN_FRONTEND=noninteractive apt install -y --option Dpkg::Options::="--force-confold" gcc-arm-linux-gnueabihf
-DEBIAN_FRONTEND=noninteractive apt install -y --option Dpkg::Options::="--force-confold" libc6-dev-armhf-cross
 
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y --option Dpkg::Options::="--force-confold" linux-firmware wireless-tools git python-is-python3 python3.10-venv libgpiod-dev libyaml-cpp-dev libbluetooth-dev openssl libssl-dev libulfius-dev liborcania-dev avahi-daemon protobuf-compiler fonts-noto-color-emoji ninja-build chrony
